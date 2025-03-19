d871b26099ede4da9adb735ddf4ab13e8390f8b4
Mustafa68686 <mirzam45@mcmaster.ca>
2025-03-19 14:06:25 -0400

    going to island

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Decider.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Decider.java
@@ -1,11 +1,16 @@
 package ca.mcmaster.se2aa4.island.teamXXX;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 
 
-
 public class Decider {
+
+    private enum Stage {
+        FIND_ISLAND, END;
+    }
+    private Stage stage = Stage.FIND_ISLAND;
 
     private enum Task {
         START, FLY, LEFT, RIGHT, SCAN, RADAR_FRONT, RADAR_LEFT, RADAR_RIGHT, STOP, RADAR
@@ -18,18 +23,55 @@ public class Decider {
     private Needle facing = Needle.E;
 
     public String[][] decide() {
-        return algorithm();
+
+        if (stage == Stage.FIND_ISLAND) {
+            return findIsland2();
+        } else {
+            task = Task.STOP;
+            return getCommand();
+        }
+        
     }
 
-    private int counter = 0;
-    public String[][] algorithm() {
-        Task[] algo ={Task.FLY, Task.SCAN, Task.RIGHT, Task.RADAR_FRONT, Task.LEFT, Task.SCAN, Task.RIGHT, Task.SCAN, Task.LEFT, Task.SCAN, Task.FLY, Task.SCAN, Task.STOP};
-        if (counter >= algo.length) {
-            task = Task.STOP;
+    /*
+    public String[][] findIsland() {
+        if ((task == Task.START)||(task == Task.FLY)) {
+            task = Task.RADAR_RIGHT;
+        } else if (found.equals("GROUND")) {
+            if (task == Task.RADAR_RIGHT) {
+                task = Task.RIGHT;
+            } else {
+                if (count == 0) {
+                    task = Task.FLY;
+                    count -= 1;
+                } else {
+                    task = Task.SCAN;
+                    stage = Stage.END;
+                }
+            }
         } else {
-            task = algo[counter];
+            task = task.FLY;
         }
-        counter += 1;
+        return getCommand();
+    }
+    */
+
+    public String[][] findIsland2() {
+            if ((task == Task.START)||(task == Task.LEFT)) {
+                task = Task.RIGHT;
+            } else if (task == Task.RIGHT) {
+                task = task.RADAR_FRONT;
+            } else if (found.equals("GROUND")) {
+                if (range < 0) {
+                    task = Task.SCAN;
+                    stage = Stage.END;
+                } else {
+                    task = Task.FLY;
+                    range -= 1;
+                }
+            } else {
+                task = Task.LEFT;
+            }
         return getCommand();
     }
 
@@ -88,10 +130,34 @@ public class Decider {
     }
 
     //analyising the response
+    //analyising the response
     private int budget = 1000;
+    private String status = "OK";
+    private String found = null;
+    //private int count = 0;
+    private int range;
     public String analyse(JSONObject response) {
         budget -= response.getInt("cost");
-        return ""+budget;
+        if (task == Task.RADAR_FRONT) {
+            return analyseRadar(response.getJSONObject("extras"));
+        } else if (task == Task.SCAN) {
+            return analyseScan(response.getJSONObject("extras"));
+        }
+        return "na";
+    }
+
+    private String analyseRadar(JSONObject extras) {
+        range = extras.getInt("range");
+        found = extras.getString("found");
+        String aa = "" + range + " " + found;
+        return aa;
+    }
+    private String analyseScan(JSONObject extras) {
+        JSONArray biomes = extras.getJSONArray("biomes");
+        JSONArray creeks = extras.getJSONArray("creeks");
+        JSONArray sites = extras.getJSONArray("sites");
+        String aa = "" + biomes.toString() + creeks.toString() + sites.toString();
+        return aa;
     }
 
 }
